bc0c1dcfefda1a5679a7c3756e8b78939c60e4f9
A.kirill <jqweryjson@gmail.com>
2017-10-13 19:03:42 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', //тег компонента (селектор) Необходимо добавлять префикс
+  templateUrl: './app.component.html',//сам шаблон который будет отрендерен внутри крмпонента
+  styleUrls: ['./app.component.scss'] //стили компонента
 })
 export class AppComponent {
   title = 'app';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { HttpModule } from "@angular/http";
+import { AppRoutingModule } from "./app-routing.module";
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ //указываем компонеты в декл�, HomeComponent�рациях
+    AppComponent, //базовый компонент с которого начинается работа прилложения
+    HomeComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [ //импортируем модули
+    BrowserModule,
+    AppRoutingModule,
+    HttpModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] //указывается базовый компонент с которго начинается рендеренинг приложения
 })
 export class AppModule { }
+
